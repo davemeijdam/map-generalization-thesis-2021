577c3c1a2455b1a6efd5d4ff0c2b75d0f214b97a
davemeijdam <davemeijdam@outlook.com>
2021-05-19 13:58:06 +0200

    Added fully connected NN with mask as input

--- utils/datasets.py
+++ utils/datasets.py
@@ -20,30 +20,16 @@ resources = [
     'y_test.p'
 ]
 
-labels_map = {
-    0: "No Change",
-    1: "Douglas-Peucker 0.5",
-    2: "Douglas-Peucker 0.1",
-    3: "Douglas-Peucker 0.05",
-    4: "Douglas-Peucker 0.01",
-    5: "Douglas-Peucker 0.005",
-    6: "Douglas-Peucker 0.001",
-    7: "Visvalingam-Whyatt 0.5",
-    8: "Visvalingam-Whyatt 0.1",
-    9: "Visvalingam-Whyatt 0.05",
-    10: "Visvalingam-Whyatt 0.01",
-    11: "Visvalingam-Whyatt 0.005",
-    12: "Visvalingam-Whyatt 0.001",
-    13: "Visvalingam-Whyatt 0.0005",
-    14: "Visvalingam-Whyatt 0.0001",
-    15: "Visvalingam-Whyatt 0.00005"
-}
-
 class GeoSpatialDataset():
 
-    def __init__(self,labels, data):
-        self.labels = pickle.load(open(str(datadir + 'y_train.p')))
-        self.data = pickle.load(open(str(datadir + 'y_train.p')))
+    def __init__(self,labels, data, train=True):
+        if train == True:
+            self.labels = pickle.load(open(str(datadir + 'y_train.p')))
+            self.data = pickle.load(open(str(datadir + 'y_train.p')))
+
+        if train == False:
+            self.labels = pickle.load(open(str(datadir + 'y_test.p')))
+            self.data = pickle.load(open(str(datadir + 'y_test.p')))
 
     def __getitem__(self, idx):
         if torch.is_tensor(idx):
